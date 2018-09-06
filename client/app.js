dcbb3c2cc6802ff35d4cc6770fb5311573f258ff
Vaibhawk <vaibhawk@in-vaibhawk.local>
2018-09-06 10:59:01 +0530

    add axios call

--- client/app.js
+++ client/app.js
@@ -1,36 +1,17 @@
 const React = require('react');
+const axios = require('axios');
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      count: 0
-    }
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
-  }
-
-  increment() {
-    const count = this.state.count + 1;
-    this.setState({
-      count 
-    })
-  }
-
-  decrement(){
-    const count = this.state.count - 1;
-    this.setState({
-      count 
+  componentDidMount(){
+    axios.get('/pets').then(res => {
+      console.log(res.data)
     })
   }
 
   render(){
     return(
       <div>
-        {
-          this.state.count
-        }
-        <button onClick={this.increment}>+</button><button onClick={this.decrement}>-</button>
+        Pets
       </div>
     )
   }
